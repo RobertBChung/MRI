07315d79e592dbfbd64c0d16f1ad5ce93977d41e
rbchung15 <rbchung@hkcoche.org>
2024-02-23 17:37:02 +0800

    1

--- helper.py
+++ helper.py
@@ -1,39 +1,16 @@
 KV = """
-ScreenManager:
-    MenuScreen:
-    ProfileScreen:
-    UploadScreen:
-
-<MenuScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: "Profile"
-        pos_hint: {'center_x': 0.5,'center_y': 0.5}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: "Upload"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'upload'
-
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: "Welcome"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: "Upload"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
+Screen:
+    MDNavigationLayout:
+        ScreenManager:
+            Screen:
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDTopAppBar:
+                        title: "Demo Application"
+                        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+                        elevation: 5
+                    Widget:
+        MDNavigationDrawer:
+            id: nav_drawer
 """
 
--- main.py
+++ main.py
@@ -1,64 +1,9 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-#from helper import KV;
+from helper import KV;
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 
-KV = """
-ScreenManager:
-    MenuScreen:
-    ProfileScreen:
-    UploadScreen:
-
-<MenuScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: "Profile"
-        pos_hint: {'center_x': 0.5,'center_y': 0.5}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: "Upload"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'upload'
-
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: "Welcome"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: "Upload"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-"""
-
-Window.size = (500, 500)
-
-    
-class MenuScreen(Screen):
-    pass
-
-class ProfileScreen(Screen):
-    pass
-
-class UploadScreen(Screen):
-    pass
-
-sm=ScreenManager()
-sm.add_widget(MenuScreen(name="menu"))
-sm.add_widget(ProfileScreen(name="profile"))
-sm.add_widget(UploadScreen(name="upload"))
 
 class DemoApp(MDApp):
     def build(self):
