ae21b0797260b5040bf7c6aea1a40f15da7e4740
rbchung15 <rbchung@hkcoche.org>
2024-02-26 11:47:14 +0800

    image

--- helper.py
+++ helper.py
@@ -1,39 +1,38 @@
-KV = """
-ScreenManager:
-    MenuScreen:
-    ProfileScreen:
-    UploadScreen:
-
-<MenuScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: "Profile"
-        pos_hint: {'center_x': 0.5,'center_y': 0.5}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: "Upload"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'upload'
-
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: "Welcome"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: "Upload"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-"""
-
+KV = KV = '''
+Screen:
+    MDNavigationLayout:
+        ScreenManager:
+            Screen:
+                MDBoxLayout:
+                    orientation: 'vertical'
+                    MDTopAppBar:
+                        title: "Demo"
+                        md_bg_color: .2, .2, .2, 1
+                        specific_text_color: 1, 1, 1, 1
+                    MDBoxLayout:
+                        orientation: "horizontal"
+                        MDBoxLayout:
+                            orientation: 'vertical'
+                            Image:
+                                source: "cat.jpg"
+                        MDBoxLayout:
+                            orientation: 'vertical'
+                            MDLabel: 
+                                text:"hello"
+                                halign: "center"
+                    MDBottomAppBar:
+                        id: bottom_appbar
+                        allow_hidden: True
+                        theme_bg_color: "Custom"
+                        md_bg_color: "#232217"
+                        halign: "center"
+                        MDFabBottomAppBarButton:
+                            id: fab_button
+                            icon: "plus"
+                            halign: "center"
+                            theme_bg_color: "Custom"
+                            md_bg_color: "#373A22"
+                            theme_icon_color: "Custom"
+                            icon_color: "#ffffff"
+                            on_press: app.selectFile()
+'''
--- main.py
+++ main.py
@@ -1,67 +1,16 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-#from helper import KV;
-from kivy.core.window import Window
-from kivy.uix.screenmanager import Screen, ScreenManager
-
-KV = """
-ScreenManager:
-    MenuScreen:
-    ProfileScreen:
-    UploadScreen:
-
-<MenuScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: "Profile"
-        pos_hint: {'center_x': 0.5,'center_y': 0.5}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: "Upload"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'upload'
-
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: "Welcome"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: "Upload"
-        halign: "center"
-    MDRectangleFlatButton:
-        text: "Menu"
-        pos_hint: {'center_x': 0.5,'center_y': 0.2}
-        on_press: root.manager.current = 'menu'
-
-"""
-
-Window.size = (500, 500)
-
-    
-class MenuScreen(Screen):
-    pass
-
-class ProfileScreen(Screen):
-    pass
-
-class UploadScreen(Screen):
-    pass
-
-sm=ScreenManager()
-sm.add_widget(MenuScreen(name="menu"))
-sm.add_widget(ProfileScreen(name="profile"))
-sm.add_widget(UploadScreen(name="upload"))
+from helper import KV;
+from tkinter.filedialog import askopenfilename
+from kivy.uix.image import Image
 
 class DemoApp(MDApp):
     def build(self):
+        # img = Image(source='cat.jpg')
+        # return img
         return Builder.load_string(KV)
+    def selectFile(self):
+        filename = askopenfilename()
+        print(filename)
 
 DemoApp().run()
